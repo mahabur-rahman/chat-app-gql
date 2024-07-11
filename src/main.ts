04ceca9b2a870827401e28eabbcd56c67a8a0b00
mahabur-rahman <annur4395@gmail.com>
2024-07-11 17:36:16 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 }
